4a38812c4d9b6b07e8093b97d6c5bd887dcdee38
Onursal Toparlak <98605584+onursaltoparlak@users.noreply.github.com>
2025-06-25 10:56:28 +0300

    last changes

---
+++ Context/WeatherContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project6_ApiWeather.Entities;
+
+namespace Project6_ApiWeather.Context
+{
+    public class WeatherContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-0AOKHL4\\SQLEXPRESS;Initial Catalog=Db6Project20;Integrated Security=true;TrustServerCertificate=true");
+
+        }
+
+        public DbSet<City> Cities { get; set; }
+    }
+}
